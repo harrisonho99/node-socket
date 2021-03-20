9de25c182e342544a53cd4a8ca0cb3dcb5634b1f
hotsnow199 <49254639+hotsnow199@users.noreply.github.com>
2021-03-21 04:48:09 +0700

    done !

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var WebSocketServer = require('websocket').server;
 var http = require('http');
 const fs = require('fs');
 const path = require('path');
-var server = http.createServer(function (request, response) {
+var server = http.createServer(function (_, response) {
   // process HTTP request. Since we're writing just WebSockets
   // server we don't have to implement anything.
   fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
@@ -13,6 +13,7 @@ var server = http.createServer(function (request, response) {
       return response.end();
     }
     response.write(data);
+    response.end();
   });
 });
 server.listen(3000, function () {
@@ -26,18 +27,53 @@ wsServer = new WebSocketServer({
 
 // WebSocket server
 wsServer.on('request', function (request) {
-  var connection = request.accept(null, request.origin);
-
+  var connection = request.accept(null, '*');
+  const dataPath = path.join(__dirname, 'message.json');
   // This is the most important callback for us, we'll handle
   // all messages from users here.
-  connection.on('message', function (message) {
-    if (message.type === 'utf8') {
+  connection.on('open', () => {
+    console.log('open connection');
+  });
+  connection.on('message', function (data) {
+    if (data.type === 'utf8') {
       // process WebSocket message
+      switch (data.utf8Data) {
+        case '':
+          fs.readFile(dataPath, (err, data) => {
+            if (!err) {
+              return connection.send(data.toString());
+            }
+          });
+          break;
+        default:
+          const message = {
+            time: Date.now(),
+            text: data.utf8Data,
+          };
+
+          fs.readFile(dataPath, (err, fileData) => {
+            if (err) {
+              const stringJSON = JSON.stringify([message]);
+              return fs.writeFile(dataPath, stringJSON, (err) => {
+                if (err) throw err;
+                connection.send(stringJSON);
+              });
+            }
+            const list = JSON.parse(fileData);
+            list.push(message);
+            stringedJson = JSON.stringify(list);
+            fs.writeFile(dataPath, stringedJson, (err) => {
+              if (err) throw err;
+              connection.send(stringedJson);
+            });
+          });
+          break;
+      }
     }
-    console.log(message);
   });
 
   connection.on('close', function (connection) {
     // close user connection
+    console.log('connection closed!');
   });
 });
